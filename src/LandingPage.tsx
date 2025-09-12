90fe9a3004ef0ed9ca51c35b43c7b3928eaaf71d
Cestamir <cestmirpavlasek@gmail.com>
2025-09-12 19:22:10 +0200

    Implemented display function for foods

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
+import { foodList } from '.';
 
+interface Food{
+    name: string
+    class: string
+}
 
 const LandingPage = () => {
 
     const [searchedItem,setSearchedItem] = useState<string>("");
+    const [displayItems,setDisplayItems] = useState<Food[]>([]);
 
     const handleOnchange = (e : any) => {
         setSearchedItem(e.target.value);
@@ -11,14 +17,26 @@ const LandingPage = () => {
 
     const handleSearch = () => {
         setSearchedItem("")
+        showItems(searchedItem)
     }
-    
+
+    const showItems = (input : string) => {
+        let filteredItems = foodList.filter((item) => {
+            if (item.name.includes(input)){
+                return item;
+            }
+        })
+        setDisplayItems(filteredItems)
+    }
 
   return (
-    <div>
-        The food bible
+    <div id='search-bar'>
+        <span>The Food Bible</span>
         <input id='search-field' value={searchedItem} onChange={handleOnchange}/>
         <button id='search-button' onClick={handleSearch}>Search</button>
+        <div id='display'>
+            {displayItems.map((item) => (<p key={item.name}>{item.name}</p>))}
+        </div>
     </div>
   )
 }
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+
+export const foodList = [{name: "cucumber",class: "vegetables"},{name: "egg",class: "dairy"},{name: "pork",class: "meat"},]
